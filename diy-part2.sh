38cf264bd29c29f1ee3bfb4e93dba63122d43380
98416 <98416@163.com>
2023-10-19 22:11:01 +0800

    新手

---
+++ diy-part2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part02.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+
+# Kernel5.4
+sed -i 's/PATCHVER:=5.15/PATCHVER:=5.4/g' target/linux/x86/Makefile
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+#修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+#软件合集
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small-package.git package/openwrt-samll
+
+#添加额外软件包
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+git clone https://github.com/iwrt/luci-app-ikoolproxy.git  package/luci-app-ikoolproxy
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
